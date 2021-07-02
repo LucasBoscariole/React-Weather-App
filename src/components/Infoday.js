4ac8527f279c53f9e2e82305a46cf9d9b70ebcab
Lucas Boscariole Silva <lucas.boscariole@hotmail.com>
2021-07-02 12:13:52 -0300

    commit

--- src/components/Infoday.js
+++ src/components/Infoday.js
@@ -17,27 +17,48 @@ function Infoday() {
     const name = data.name;
     const { speed } = data.wind;
     const { description, icon } = data.weather[0];
-    return <article></article>;
+    const { country } = data.sys;
+    return (
+      <article className='container-day'>
+        <div className='container-icon'>
+          {icon === '01d' && <FaSun className='icon sun' />}
+          {icon === '01n' && <FaSun className='icon sun' />}
+          {icon === '02d' && <FaCloudSun className='icon cloud-sun' />}
+          {icon === '02n' && <FaCloudSun className='icon cloud-sun-night' />}
+          {icon === '03d' && <FaCloud className='icon cloud' />}
+          {icon === '03n' && <FaCloud className='icon cloud-night' />}
+          {icon === '04d' && <FaCloud className='icon cloud' />}
+          {icon === '04n' && <FaCloud className='icon cloud-night' />}
+          {icon === '09d' && <FaCloudRain className='icon rain' />}
+          {icon === '09n' && <FaCloudRain className='icon rain-night' />}
+          {icon === '10d' && <FaCloudSunRain className='icon sun-rain' />}
+          {icon === '10n' && <FaCloudSunRain className='icon sun-rain' />}
+          {icon === '11d' && <IoThunderstormSharp className='icon storm' />}
+          {icon === '11n' && <IoThunderstormSharp className='icon storm' />}
+          {icon === '13d' && <FaSnowflake className='icon snow' />}
+          {icon === '13n' && <FaSnowflake className='icon snow' />}
+          {icon === '50d' && <FaCloud className='icon cloud' />}
+          {icon === '50n' && <FaCloud className='icon cloud-night' />}
+        </div>
+        <div className='line'></div>
+        <div className='container-info'>
+          <h2 className='color-title'>
+            {name}, <span>{country}</span>
+          </h2>
+          <h4 className='color-subtitle desc'>{description}</h4>
+          <p className='color-subtitle'>Wind: {speed}</p>
+          <p className='color-subtitle'>Feels Like: {feels_like}°C</p>
+          <div className='flex-container'>
+            <h4 className='color-subtitle'>{temp_min}°C</h4>
+            <h3 className='color-title temp'>{temp}°C</h3>
+            <h4 className='color-subtitle'>{temp_max}°C</h4>
+          </div>
+        </div>
+      </article>
+    );
   } else {
     return <article></article>;
   }
 }
 
 export default Infoday;
-
-//             {icon === '01d' && <FaSun className='icon sun' />}
-//             {icon === '01n' && <FaSun className='icon sun' />}
-//             {icon === '02d' && <FaCloudSun className='icon cloud-sun' />}
-//             {icon === '02n' && <FaCloudSun className='icon cloud-sun-night' />}
-//             {icon === '03d' && <FaCloud className='icon cloud' />}
-//             {icon === '03n' && <FaCloud className='icon cloud-night' />}
-//             {icon === '04d' && <FaCloud className='icon cloud' />}
-//             {icon === '04n' && <FaCloud className='icon cloud-night' />}
-//             {icon === '09d' && <FaCloudRain className='icon rain' />}
-//             {icon === '09n' && <FaCloudRain className='icon rain-night' />}
-//             {icon === '10d' && <FaCloudSunRain className='icon sun-rain' />}
-//             {icon === '10n' && <FaCloudSunRain className='icon sun-rain' />}
-//             {icon === '11d' && <IoThunderstormSharp className='icon storm' />}
-//             {icon === '11n' && <IoThunderstormSharp className='icon storm' />}
-//             {icon === '13d' && <FaSnowflake className='icon snow' />}
-//             {icon === '13n' && <FaSnowflake className='icon snow' />}
